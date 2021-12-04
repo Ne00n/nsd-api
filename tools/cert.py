b7121062b14a6b47c9af719118edba079bf54158
Neoon <neo@tiefkuehler-madness.me>
2021-12-04 18:14:48 +0100

    dynamic path

--- tools/cert.py
+++ tools/cert.py
@@ -2,8 +2,9 @@ import requests, json, time, sys, os
 from random import randint
 from datetime import datetime
 
+path = os.getcwd()
 print("Loading domains.json")
-with open('domains.json') as f:
+with open(f"{path}/domains.json") as f:
     domains = json.load(f)
 
 if not os.path.isdir("certs"): os.mkdir('certs')
@@ -22,7 +23,7 @@ def splitDomain(domain):
     else:
         return ".".join(parts[0:len(parts)-2]),".".join(parts[len(parts)-2:])
 
-def getCert(domains,fullDomain):
+def getCert(domains,fullDomain,path):
     print(f"Getting Certificate for {fullDomain}")
     endpoint = domains['remote'][0]
     subdomain,domain = splitDomain(fullDomain)
@@ -30,9 +31,9 @@ def getCert(domains,fullDomain):
     cert = fetch(domain,url)
     if cert:
         print(f"Saving Certificate for {fullDomain}")
-        with open(f"certs/{fullDomain}-fullchain.pem", 'w') as out:
+        with open(f"{path}/certs/{fullDomain}-fullchain.pem", 'w') as out:
             out.write(cert['success']['fullchain'])
-        with open(f"certs/{fullDomain}-privkey.pem", 'w') as out:
+        with open(f"{path}/certs/{fullDomain}-privkey.pem", 'w') as out:
             out.write(cert['success']['privkey'])
         os.system("sudo /bin/systemctl reload nginx")
     else:
@@ -40,15 +41,15 @@ def getCert(domains,fullDomain):
 
 for fullDomain in domains['domains']:
     print(f"Checking {fullDomain}")
-    if not os.path.isfile(f"certs/{fullDomain}-fullchain.pem") or not os.path.isfile(f"certs/{fullDomain}-privkey.pem"):
+    if not os.path.isfile(f"{path}/certs/{fullDomain}-fullchain.pem") or not os.path.isfile(f"{path}/certs/{fullDomain}-privkey.pem"):
         print(f"Certificate not found for {fullDomain}")
-        getCert(domains,fullDomain)
+        getCert(domains,fullDomain,path)
     else:
         print(f"Certificate found for {fullDomain}")
         print(f"Checking Certificate age for {fullDomain}")
-        if os.path.getmtime(f"certs/{fullDomain}-fullchain.pem") + (86400 * 30) < datetime.now().timestamp():
+        if os.path.getmtime(f"{path}/certs/{fullDomain}-fullchain.pem") + (86400 * 30) < datetime.now().timestamp():
             print(f"Certificate for {fullDomain} is older than 30 Days, renewing")
-            getCert(domains,fullDomain)
+            getCert(domains,fullDomain,path)
         else:
             print(f"Skipping {fullDomain}")
     time.sleep(randint(60,120))
